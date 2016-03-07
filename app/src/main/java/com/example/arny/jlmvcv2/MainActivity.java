33bebb6b64ff219893ebc3c167b1dcccd683a26d
JTMcs <joelmalleck@gmail.com>
2016-03-07 10:54:37 -0500

    new and improved

--- app/src/main/java/com/example/arny/jlmvcv2/MainActivity.java
+++ app/src/main/java/com/example/arny/jlmvcv2/MainActivity.java
@@ -14,13 +14,15 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    int picID, sndID, nRan, nRan2, nView, wrongCount, rightCount, totalCount;
+    int picID, sndID, nRan, nRan2, nView, rightCount, totalCount;
     ImageButton imbA1, imbA2, imbA3, imbA4;
     Button btnCh, btnSnd;
-    String[] arsAnml = {"chicken", "cow", "dog", "goose", "lion", "tiger"};
-    String[]arsKeep;
+    String[] arsAnml = {"chicken", "cow", "dog", "goose", "lion", "snake", "tiger"};
+    String[] arsKeep;
+    String sScore;
     ArrayList<String> listAnml;
     Toast tCor, tBad;
+    TextView txtScore;
     MediaPlayer mNoise;
 
     @Override
@@ -32,22 +34,24 @@ public class MainActivity extends AppCompatActivity {
 
         arsKeep = new String[4];
 
-        btnCh = (Button) findViewById(R.id.button);
+        txtScore = (TextView) findViewById(R.id.tvScore);
+
+        btnCh = (Button) findViewById(R.id.btn1);
         btnCh.setOnClickListener(Change);
 
-        btnSnd = (Button) findViewById(R.id.button2);
+        btnSnd = (Button) findViewById(R.id.btn2);
         btnSnd.setOnClickListener(Sound);
 
-        imbA1 = (ImageButton) findViewById(R.id.imageButton);
+        imbA1 = (ImageButton) findViewById(R.id.imgBtn1);
         imbA1.setOnClickListener(imgClk);
 
-        imbA2 = (ImageButton) findViewById(R.id.imageButton2);
+        imbA2 = (ImageButton) findViewById(R.id.imgBtn2);
         imbA2.setOnClickListener(imgClk);
 
-        imbA3 = (ImageButton) findViewById(R.id.imageButton3);
+        imbA3 = (ImageButton) findViewById(R.id.imgBtn3);
         imbA3.setOnClickListener(imgClk);
 
-        imbA4 = (ImageButton) findViewById(R.id.imageButton4);
+        imbA4 = (ImageButton) findViewById(R.id.imgBtn4);
         imbA4.setOnClickListener(imgClk);
 
         tCor = Toast.makeText(getApplicationContext(), "Correct!", Toast.LENGTH_SHORT);
@@ -58,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     View.OnClickListener Change = new View.OnClickListener() {
-        public void onClick(View v){
+        public void onClick(View v) {
             listAnml = new ArrayList(Arrays.asList(arsAnml));
             nRan2 = (int) Math.floor(Math.random() * 4);
             imbA1.setBackgroundResource(getRes(0));
@@ -80,26 +84,21 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             if (v == imbA1) {
                 nView = 0;
-            }
-            else if (v == imbA2) {
+            } else if (v == imbA2) {
                 nView = 1;
-            }
-            else if (v == imbA3) {
+            } else if (v == imbA3) {
                 nView = 2;
-            }
-            else {
+            } else {
                 nView = 3;
             }
-            if (nView == nRan) {
+            if (nView == nRan2) {
                 rightCount++;
                 tCor.show();
                 mNoise.stop();
                 mNoise.release();
                 btnCh.performClick();
                 btnSnd.performClick();
-            }
-            else {
-                wrongCount++;
+            } else {
                 tBad.show();
                 mNoise.stop();
                 mNoise.release();
@@ -107,18 +106,21 @@ public class MainActivity extends AppCompatActivity {
                 btnSnd.performClick();
             }
             totalCount++;
+            sScore = rightCount + " / " + totalCount;
+            txtScore.setText(sScore);
         }
     };
 
-    public int getRes(int nPic){
+    public int getRes(int nPic) {
         nRan = (int) Math.floor(Math.random() * listAnml.size());
         picID = getResources().getIdentifier(listAnml.get(nRan), "drawable", getPackageName());
         arsKeep[nPic] = listAnml.get(nRan);
         listAnml.remove(nRan);
         return picID;
     }
-    public void playSound(){
-        sndID = getResources().getIdentifier(arsKeep[nRan], "raw", getPackageName());
+
+    public void playSound() {
+        sndID = getResources().getIdentifier(arsKeep[nRan2], "raw", getPackageName());
         mNoise = MediaPlayer.create(getApplicationContext(), sndID);
         mNoise.start();
     }
